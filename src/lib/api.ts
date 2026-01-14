38ed8f3b99e2080bb3d88c68d5d7db45e9d8c85e
Hannaamal <hannaamal2715@gmail.com>
2026-01-14 09:26:19 +0530

    api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,45 +4,18 @@ import Cookies from "js-cookie";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
-  timeout: 10000, // 10 second timeout
-  // Additional cookie configurations
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  }
 });
 
-// Add interceptor to log requests for debugging
+// Add interceptor OUTSIDE axios.create()
 api.interceptors.request.use((config) => {
-  console.log('API Request:', {
-    url: config.url,
-    method: config.method,
-    withCredentials: config.withCredentials,
-    headers: config.headers
-  });
-  
-  // Since auth_token is HTTP-only, we don't read it here
-  // The cookie will be automatically sent with withCredentials: true
+ const token = Cookies.get("auth_token"); // FIXED
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
-// Add response interceptor to log responses for debugging
-api.interceptors.response.use(
-  (response) => {
-    console.log('API Response:', {
-      status: response.status,
-      url: response.config.url
-    });
-    return response;
-  },
-  (error) => {
-    console.log('API Error:', {
-      status: error.response?.status,
-      url: error.config?.url,
-      message: error.message
-    });
-    return Promise.reject(error);
-  }
-);
-
 export default api;
+
