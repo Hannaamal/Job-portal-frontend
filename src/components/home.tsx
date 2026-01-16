a43bb7934951948487fa56ed9eb134c67739fdde
Hannaamal <hannaamal2715@gmail.com>
2026-01-16 12:06:39 +0530

    console

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,7 +4,9 @@ import { Suspense } from "react";
 import HomeContent from "./homeContent";
 
 export default function HomePage() {
+  console.log("Rendering HomePage");
   return (
+
     <Suspense fallback={<div className="p-6">Loading jobs...</div>}>
       <HomeContent />
     </Suspense>
